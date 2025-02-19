460f9ecae8a33616b1cebba940a2c71ee77b99d8
Gaurav Phogat <92914327+Gaurav-Phogat@users.noreply.github.com>
2025-02-19 21:55:16 +0530

    Time: 6 ms (56.66%), Space: 45 MB (56.16%) - LeetHub

---
+++ 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findNonMinOrMax(int[] nums) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for(int n : nums){
+            minHeap.offer(n);
+            if(minHeap.size() == 3) {
+                minHeap.poll();
+                return minHeap.peek();
+            };
+        }
+        return -1;
+    }
+}
