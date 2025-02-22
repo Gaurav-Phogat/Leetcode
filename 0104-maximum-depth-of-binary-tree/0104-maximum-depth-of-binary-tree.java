d71ab16f8b1841d991414f5d7f9fcb1f1ab2862b
Gaurav Phogat <92914327+Gaurav-Phogat@users.noreply.github.com>
2025-02-22 12:02:40 +0530

    Time: 1 ms (12.24%), Space: 42.6 MB (60.73%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -16,51 +16,25 @@
 class Solution {
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
-        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
 
-     /* if(root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+        while(!queue.isEmpty()){
+            level++;
 
-      Queue<TreeNode> queue = new LinkedList<>();
-      queue.add(root);
-      int depth = 0;
+            int levelSize = queue.size();
 
-      while(!queue.isEmpty()){
-        depth++;
-        int levelSize = queue.size();
-        for(int i = 0; i < levelSize;i++){
-
-            TreeNode temp = queue.poll();
-
-            if(temp.left != null){
-                queue.add(temp.left);
-            }
-            if(temp.right != null){
-                queue.add(temp.right);
+            for(int i = 0;i < levelSize;i++){
+                TreeNode temp = queue.poll();
+                if(temp.left != null){
+                    queue.add(temp.left);
+                }
+                if(temp.right != null){
+                    queue.add(temp.right);
+                }
             }
         }
-      }
-      return depth;
-      */
-     /*
-      if(root == null) return 0;
-
-      Stack<Pair<TreeNode,Integer>> stack = new Stack<>();
-
-      stack.push(new Pair<>(root, 1));
-      int maxDepth = 0;
-
-      while (!stack.isEmpty()) {
-
-        Pair<TreeNode,Integer> curr = stack.pop();
-        TreeNode node = curr.getKey();
-        int depth = curr.getValue();
-
-        if(node != null){
-            maxDepth = Math.max(maxDepth,depth);
-            stack.push(new Pair<>(node.left,depth+1));
-            stack.push(new Pair<>(node.right,depth+1));
-        }
-      }
-      return maxDepth; */
+    return level;
     }
 }
