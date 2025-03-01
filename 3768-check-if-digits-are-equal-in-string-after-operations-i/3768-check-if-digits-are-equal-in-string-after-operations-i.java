77818c816ea3a25eb06c6a5e373d3768e8c50487
Gaurav Phogat <92914327+Gaurav-Phogat@users.noreply.github.com>
2025-03-01 12:40:57 +0530

    Time: 8 ms (79.38%), Space: 42.4 MB (95.72%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
@@ -1,24 +1,19 @@
 class Solution {
     public boolean hasSameDigits(String s) {
         StringBuilder sb = new StringBuilder(s);
-        while(sb.length() > 2){
-            StringBuilder curr = new StringBuilder();
+        
+        while (sb.length() > 2) {
             int size = sb.length();
             
-            for(int i = 0;i < size-1;i++){
-                int total = Character.getNumericValue(sb.charAt(i));
-
-                if(i+1 < size){
-                    total += Character.getNumericValue(sb.charAt(i+1));
-                }
-                curr.append(total%10);
+            // Reuse the existing StringBuilder
+            for (int i = 0; i < size - 1; i++) {
+                int total = Character.getNumericValue(sb.charAt(i)) + Character.getNumericValue(sb.charAt(i + 1));
+                sb.setCharAt(i, (char) ('0' + (total % 10)));
             }
-            sb.setLength(0);
-            sb.append(curr);
+            
+            sb.setLength(size - 1); // Reduce the length of sb
         }
-        int check1 = (int) sb.charAt(0);
-        int check2 = (int) sb.charAt(1);
-        if(check1 == check2) return true;
-        return false;
+        
+        return sb.length() == 2 && sb.charAt(0) == sb.charAt(1);
     }
 }
