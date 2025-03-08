bf362e5e126ef05d2d11fef27729ab24b77a0895
Gaurav Phogat <92914327+Gaurav-Phogat@users.noreply.github.com>
2025-03-08 11:17:48 +0530

    Time: 3 ms (21.6%), Space: 55.1 MB (93.83%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -4,16 +4,14 @@ class Solution {
         int[] suffix = new int[nums.length];
         Arrays.fill(prefix,1);
         Arrays.fill(suffix,1);
-        int n = nums.length;
 
-        for(int i = 1;i < n;i++){
+        for(int i = 1; i < nums.length;i++){
             prefix[i] = prefix[i-1]*nums[i-1];
         }
-        for(int i = n-2;i>=0;i--){
+        for(int i = nums.length-2;i >= 0;i--){
             suffix[i] = suffix[i+1]*nums[i+1];
         }
-
-        for(int i = 0;i < n ;i++){
+        for(int i = 0;i < nums.length;i++){
             nums[i] = prefix[i]*suffix[i];
         }
         return nums;
