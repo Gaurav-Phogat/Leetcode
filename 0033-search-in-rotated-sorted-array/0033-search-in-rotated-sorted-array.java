5645fffd010b7c3ea35c13795dcb70c1c975e8fd
Gaurav Phogat <92914327+Gaurav-Phogat@users.noreply.github.com>
2025-03-21 23:32:49 +0530

    Time: 0 ms (100%), Space: 42.1 MB (51.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -7,17 +7,17 @@ class Solution {
 
             if(nums[mid] == target) return mid;
 
-            else if(nums[left] <= nums[mid]){
+            else if(nums[mid] >= nums[left]){
                 if(nums[left] <= target && target < nums[mid]){
-                    right = mid-1;
+                    right = mid - 1;
                 } else {
-                    left = mid+1;
+                    left = mid + 1;
                 }
             } else {
                 if(nums[mid] < target && target <= nums[right]){
-                   left = mid+1; 
+                    left = mid + 1;
                 } else {
-                    right = mid-1;
+                    right = mid - 1;
                 }
             }
         }
