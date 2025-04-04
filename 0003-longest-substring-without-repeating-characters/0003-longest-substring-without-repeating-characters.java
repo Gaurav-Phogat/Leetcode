362984104c631a1160d99852c307dc89dc03827c
Gaurav Phogat <92914327+Gaurav-Phogat@users.noreply.github.com>
2025-04-04 15:05:16 +0530

    Time: 2 ms (98.54%), Space: 43.1 MB (96.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,17 +1,17 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int[] chars = new int[128];
-        int longest = 0;
-        int start = 0;
         Arrays.fill(chars,-1);
+        int ans = 0;
+        int start = 0;
 
-        for(int i = 0; i < s.length(); i++){
+        for(int i = 0;i < s.length();i++){
             if(chars[s.charAt(i)] >= start){
                 start = chars[s.charAt(i)]+1;
             }
             chars[s.charAt(i)] = i;
-            longest = Math.max(longest,(i-start+1));
+            ans = Math.max(ans,i-start+1);
         }
-        return longest;
+        return ans;
     }
 }
