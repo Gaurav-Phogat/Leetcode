71ef0a730b94e16602d2e86cff2690ce7122a18d
Gaurav Phogat <92914327+Gaurav-Phogat@users.noreply.github.com>
2025-04-08 22:58:26 +0530

    Time: 4 ms (7.15%), Space: 56.3 MB (15.99%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -7,12 +7,10 @@ class Solution {
 
         for(int i = 1;i < nums.length;i++){
             prefix[i] = prefix[i-1]*nums[i-1];
-            System.out.println(prefix[i]);
         }
 
         for(int i = nums.length-2;i >= 0;i--){
             suffix[i] = suffix[i+1]*nums[i+1];
-            System.out.println(suffix[i]);
         }
 
         for(int i = 0;i < nums.length;i++){
