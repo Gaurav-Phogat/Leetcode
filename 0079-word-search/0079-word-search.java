bf0ab0428c458cd95abd4783c188d3aebd4a23c8
Gaurav Phogat <92914327+Gaurav-Phogat@users.noreply.github.com>
2025-04-13 15:08:49 +0530

    Time: 140 ms (66.35%), Space: 41.6 MB (78.2%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,42 +1,38 @@
-public class Solution {
+class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
 
-        boolean[][] visited = new boolean[m][n];
-        boolean result = false;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    result = backtrack(board, word, visited, i, j, 0);
-                    if (result) return true;
+        boolean ans = false;
+
+        for(int i = 0;i < board.length;i++){
+            for(int j = 0;j < board[0].length;j++){
+                if(board[i][j] == word.charAt(0)){
+                    boolean temp = check(board,new boolean[board.length][board[0].length],word,i,j,0);
+                    if(temp == true){
+                        ans = true;
+                    }
                 }
             }
         }
-        
-        return false;
+        return ans;
     }
-    
-    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
-        if (index == word.length()) {
-            return true;
-        }
+    public boolean check(char[][] board,boolean[][] matrix,String word,int i,int j,int index){
+
+        if(i >= board.length || i < 0 || j >= board[0].length || j < 0 ||
+            word.charAt(index) != board[i][j] || matrix[i][j] == true
+        ) return false;
+
+        matrix[i][j] = true;
+
+        if(word.length()-1 == index) return true;
+
+        boolean found = (
+            check(board,matrix,word,i-1,j,index+1) ||
+            check(board,matrix,word,i+1,j,index+1) ||
+            check(board,matrix,word,i,j-1,index+1) ||
+            check(board,matrix,word,i,j+1,index+1));
         
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
-            return false;
-        }
-        
-        visited[i][j] = true;
-        
-        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
-            backtrack(board, word, visited, i - 1, j, index + 1) ||
-            backtrack(board, word, visited, i, j + 1, index + 1) ||
-            backtrack(board, word, visited, i, j - 1, index + 1)) {
-            return true;
-        }
-        
-        visited[i][j] = false;
-        return false;
+        matrix[i][j] = false;
+
+        return found;
     }
 }
