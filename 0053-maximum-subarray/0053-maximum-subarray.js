b4f77b2d58727fa03446661c6908d23670828624
Gaurav Phogat <92914327+Gaurav-Phogat@users.noreply.github.com>
2025-04-28 17:51:09 +0530

    Time: 6 ms (13.08%), Space: 65.5 MB (16.22%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -6,17 +6,13 @@ var maxSubArray = function(nums) {
     let maxSum = Number.MIN_SAFE_INTEGER;
     let curSum = 0;
 
-    for(let n of nums){
-        maxSum = Math.max(maxSum,n);
-    }
-
-    for(let n of nums){
+    for (let n of nums) {
         curSum += n;
-        maxSum = Math.max(curSum,maxSum);
-
-        if(curSum < 0){
+        maxSum = Math.max(maxSum, curSum);
+        if (curSum < 0) {
             curSum = 0;
         }
     }
+
     return maxSum;
 };
