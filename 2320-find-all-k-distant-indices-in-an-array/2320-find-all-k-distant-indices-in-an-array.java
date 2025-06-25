461c175205cf51986dba010e918d48693c6fd17e
Gaurav Phogat <92914327+Gaurav-Phogat@users.noreply.github.com>
2025-06-25 16:46:51 +0530

    Time: 95 ms (14.44%), Space: 45.3 MB (32.35%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
@@ -1,17 +1,18 @@
 class Solution {
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
-        Set<Integer> set = new TreeSet<>();
+        Set<Integer> set = new LinkedHashSet<>();
         List<Integer> list = new ArrayList<>();
 
         for(int i = 0;i < nums.length;  i++){
             if(nums[i] == key){
-                set.add(i); // 2
-                for(int j = i-1; (j > -1) && j >= (i - k); j--){
+                for(int j = ((i-k) > -1) ? (i-k) : 0 ; j < i ; j++){
                     set.add(j);
                 }
-                for(int j = i; j < nums.length && j <= (i + k);j++){
+                set.add(i);
+                for(int j = ((i+1) < nums.length) ? i+1 : nums.length-1 ; j < nums.length && j <= i+k ;j++ ){
                     set.add(j);
                 }
+                
             }
         }
 
