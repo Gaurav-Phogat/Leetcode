31790e3a5dadedbf513606228a87d9a36fc9d330
Gaurav Phogat <92914327+Gaurav-Phogat@users.noreply.github.com>
2025-06-25 16:49:19 +0530

    Time: 94 ms (14.97%), Space: 45.8 MB (7.22%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
@@ -5,14 +5,8 @@ class Solution {
 
         for(int i = 0;i < nums.length;  i++){
             if(nums[i] == key){
-                for(int j = ((i-k) > -1) ? (i-k) : 0 ; j < i ; j++){
-                    set.add(j);
-                }
-                set.add(i);
-                for(int j = ((i+1) < nums.length) ? i+1 : nums.length-1 ; j < nums.length && j <= i+k ;j++ ){
-                    set.add(j);
-                }
-                
+                for(int j = ((i-k) > -1) ? i-k : 0;j <= (i+k) && j < nums.length;j++)
+                set.add(j);
             }
         }
 
